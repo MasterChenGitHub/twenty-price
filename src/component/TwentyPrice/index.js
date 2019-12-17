6e1b7c30652b4505294d815ffad6ac645666da76
MasterChen_Zhenfeng <czfgreat@gmail.com>
2019-12-17 09:59:04 +0800

    format code

--- src/component/TwentyPrice/index.js
+++ src/component/TwentyPrice/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import StockList from "../StockList/index";
 import './index.css';
-import StockPrice from "../StockPrice";
 
 class TwentyPrice extends React.PureComponent {
 
     render() {
         return (<div className="container">
             <StockList/>
-            {/*<StockPrice/>*/}
         </div>);
     }
 
